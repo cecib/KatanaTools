934f36a2fc04a7b8502a879ec491ae4445ff8d2c
ceci <cecilia_berriz@brown.edu>
2023-01-09 10:23:42 -0400

    Refactor

--- SuperTools/AlembicLoader/v1/Editor.py
+++ SuperTools/AlembicLoader/v1/Editor.py
@@ -7,34 +7,37 @@ from Katana import UI4
 
 
 class AlembicLoaderEditor(QtWidgets.QWidget):
-
     def __init__(self, parent, node):
         QtWidgets.QWidget.__init__(self, parent)
 
         self.__node = node
 
         # Create UI parameters
-        location_parameter = self.__node.getParameter('location')
-        folder_path_parameter = self.__node.getParameter('folderPath')
+        location_parameter = self.__node.getParameter("location")
+        folder_path_parameter = self.__node.getParameter("folderPath")
 
         create_parameter_policy = UI4.FormMaster.CreateParameterPolicy
         self.__location_parameter_policy = create_parameter_policy(
-            None, location_parameter)
-
+            None, location_parameter
+        )
         self.__folder_path_parameter_policy = create_parameter_policy(
-            None, folder_path_parameter)
+            None, folder_path_parameter
+        )
 
         # Create UI widgets and layout
         widget_factory = UI4.FormMaster.KatanaFactory.ParameterWidgetFactory
         location_widget = widget_factory.buildWidget(
-            self, self.__location_parameter_policy)
+            self, self.__location_parameter_policy
+        )
         folder_path_widget = widget_factory.buildWidget(
-            self, self.__folder_path_parameter_policy)
-
+            self, self.__folder_path_parameter_policy
+        )
         self.__load_button = UI4.Widgets.ToolbarButton(
-            'Load Alembics', self,
-            UI4.Util.IconManager.GetPixmap('Icons/editText16.png'),
-            rolloverPixmap=UI4.Util.IconManager.GetPixmap('Icons/editTextHilite16.png'))
+            "Load Alembics",
+            self,
+            UI4.Util.IconManager.GetPixmap("Icons/editText16.png"),
+            rolloverPixmap=UI4.Util.IconManager.GetPixmap("Icons/editTextHilite16.png"),
+        )
         self.__load_button.clicked.connect(self.__load_button_clicked)
 
         self.main_layout = QtWidgets.QVBoxLayout()
@@ -44,18 +47,20 @@ class AlembicLoaderEditor(QtWidgets.QWidget):
 
         self.setLayout(self.main_layout)
 
-        self.combo_boxes = {}
-        self.check_boxes = {}
-        self.geo_names = []
+        self.__combo_boxes = {}
+        self.__check_boxes = {}
 
     def __load_button_clicked(self):
-        nodes = self.__node.load_alembics(self.__folder_path_parameter_policy.getValue())
-        for node in nodes:
-            node_name = node.getName()
-            geo_name = node_name[0:len(node_name)-5]
-            self.geo_names.append(geo_name)
+        loaded_geo_names = []
 
-            check_box = self.check_boxes.get(geo_name)
+        for node in self.__node.load_alembics(
+            self.__folder_path_parameter_policy.getValue()
+        ):
+            geo_name = node.getParameter("geoName")
+            loaded_geo_names.append(geo_name)
+
+            # Add node-specific parameters to UI widget
+            check_box = self.__check_boxes.get(geo_name)
             if not check_box:
                 check_box = QtWidgets.QCheckBox(geo_name + " enabled", self)
                 check_box.setChecked(True)
@@ -63,33 +68,38 @@ class AlembicLoaderEditor(QtWidgets.QWidget):
                     lambda state, name=geo_name: self.__check_box_clicked(state, name)
                 )
                 self.main_layout.addWidget(check_box)
-                self.check_boxes.update({geo_name: check_box})
+                self.__check_boxes.update({geo_name: check_box})
 
-            combo_box = self.combo_boxes.get(geo_name)
+            combo_box = self.__combo_boxes.get(geo_name)
             if not combo_box:
                 combo_box = QtWidgets.QComboBox(self)
                 combo_box.currentTextChanged.connect(
                     lambda value, name=geo_name: self.__combobox_changed(value, name)
                 )
                 self.main_layout.addWidget(combo_box)
-                self.combo_boxes.update({geo_name: combo_box})
+                self.__combo_boxes.update({geo_name: combo_box})
             combo_box.addItem(node.getName().split("_")[-1])
 
-        for geo_name in self.geo_names:
-            latest_version = max(self.__node.get_versions(geo_name))
-            combo_box = self.combo_boxes.get(geo_name)
-            combo_box.setCurrentText("v00" + str(latest_version))
+        # Set latest version for each alembic
+        for geo_name in loaded_geo_names:
+            combo_box = self.__combo_boxes.get(geo_name)
+            combo_box.setCurrentText(
+                "v" + str(max(self.__node.get_versions(geo_name))).zfill(3)
+            )
 
     def __check_box_clicked(self, state, geo_name):
-        combo_box = self.combo_boxes.get(geo_name)
+        combo_box = self.__combo_boxes.get(geo_name)
+        if combo_box is None:
+            return
         node_name = geo_name + "_" + str(combo_box.currentText())
         if state == QtCore.Qt.Checked:
             self.__node.enable_node(node_name)
         else:
             self.__node.disable_node(node_name)
 
-    def __combobox_changed(self, value, geo_name):
+    def __combobox_changed(self, version_label, geo_name):
         self.__node.update_version(
-            int(value[1:]), geo_name,
-            self.check_boxes.get(geo_name).isChecked()
+            int(version_label[1:]),
+            geo_name,
+            self.__check_boxes.get(geo_name).isChecked(),
         )
--- SuperTools/AlembicLoader/v1/Node.py
+++ SuperTools/AlembicLoader/v1/Node.py
@@ -11,26 +11,25 @@ class AlembicLoaderNode(NodegraphAPI.SuperTool):
 
     def __init__(self):
         self.hideNodegraphGroupControls()
-
         self.addOutputPort("output")
 
-        self.getParameters().createChildString('location', '/root')
-        self.getParameters().createChildString('folderPath', r'C:\Users\Ceci\Documents\_jobs\spinvfx\katana_test_products\products')
+        self.getParameters().createChildString("location", "/root")
+        self.getParameters().createChildString("folderPath", "")
 
-        self.merge_node = NodegraphAPI.CreateNode('Merge', self)
-        self.add_node_reference_param('node_merge', self.merge_node)
-
+        self.merge_node = NodegraphAPI.CreateNode("Merge", self)
+        self.merge_pos = NodegraphAPI.GetNodePosition(self.merge_node)
+        self.add_node_reference_param("node_merge", self.merge_node)
         self.getReturnPort(self.getOutputPortByIndex(0).getName()).connect(
-            self.merge_node.getOutputPortByIndex(0))
-
-        self.__abc_nodes = {}    # {name:[1,2,5]}
+            self.merge_node.getOutputPortByIndex(0)
+        )
+        self.__name_to_versions = {}
 
     def add_node_reference_param(self, param_name, node):
         param = self.getParameter(param_name)
         if not param:
-            param = self.getParameters().createChildString(param_name, '')
+            param = self.getParameters().createChildString(param_name, "")
 
-        param.setExpression('getNode(%r).getNodeName()' % node.getName())
+        param.setExpression("getNode(%r).getNodeName()" % node.getName())
 
     def disable_node(self, node_name):
         node = self.get_ref_node(node_name)
@@ -43,60 +42,52 @@ class AlembicLoaderNode(NodegraphAPI.SuperTool):
             node.setBypassed(False)
 
     def get_ref_node(self, node_name):
-        p = self.getParameter('node_' + node_name)
+        p = self.getParameter("node_" + node_name)
         if not p:
             return None
-
         return NodegraphAPI.GetNode(p.getValue(0))
 
     def get_versions(self, geo_name):
-        return self.__abc_nodes.get(geo_name)
+        return self.__name_to_versions.get(geo_name)
 
-    def load_alembics(self, rootdir):
-        alembic_nodes = []
-        merge_pos = NodegraphAPI.GetNodePosition(self.merge_node)
-        for root, subdirs, files in os.walk(rootdir):
-            for idx, filename in enumerate(files):
+    def load_alembics(self, directory):
+        for idx, filename in enumerate(os.listdir(directory)):
+            if not filename.endswith(".abc"):
+                continue
 
-                if not filename.endswith(".abc"):
-                    continue
+            geo_name = re.match(self.REGEX_NAME, filename).groups()[0]
+            version = re.split(self.REGEX_VERSION, filename)[1]
 
-                fullpath = root + os.sep + filename
+            node = NodegraphAPI.CreateNode("Alembic_In", self)
+            node_name = geo_name + "_" + str(version)
+            node.setName(node_name)
 
-                geo_name = re.match(AlembicLoaderNode.REGEX_NAME, filename).groups()[0]
-                version = re.split(AlembicLoaderNode.REGEX_VERSION, filename)[1]
+            # Update Alembic_In node parameters
+            node.getParameter("name").setValue("/root/world/" + geo_name, 1.0)
+            node.getParameter("abcAsset").setValue(
+                os.path.join(directory, filename), 1.0
+            )
+            studio_params = node.getParameters().createChildGroup("studio")
+            studio_params.createChildNumber("geoName", geo_name)
 
-                node = NodegraphAPI.CreateNode("Alembic_In", self)
-                node_name = geo_name + "_" + str(version)
-                node.setName(node_name)
+            node.getOutputPortByIndex(0).connect(self.merge_node.addInputPort(geo_name))
+            NodegraphAPI.SetNodePosition(node, (0, self.merge_pos[1] + 50 * (idx + 1)))
 
-                node.getParameter('name').setValue('/root/world/' + geo_name, 1.0)
-                node.getParameter('abcAsset').setValue(fullpath, 1.0)
-
-                root_param = node.getParameters()
-                studio_params = root_param.createChildGroup("studio")
-                studio_params.createChildNumber("version", int(version[1:]))
-
-                merge_port = self.merge_node.addInputPort(geo_name)
-                node.getOutputPortByIndex(0).connect(merge_port)
-                NodegraphAPI.SetNodePosition(node, (0, merge_pos[1]+50*(idx+1)))
-
-                version_value = int(version[1:])
-                current_versions = self.__abc_nodes.get(geo_name)
-                if current_versions:
-                    current_versions.append(version_value)
-                else:
-                    self.__abc_nodes.update({geo_name: [version_value]})
-
-                self.add_node_reference_param('node_' + node_name, node)
-                alembic_nodes.append(node)
-
-        return alembic_nodes
-
-    def update_version(self, version_num, geo_name, is_enabled):
-        node_name = geo_name + "_v00" + str(version_num)
-        for version in self.__abc_nodes.get(geo_name):
-            if version_num == version and is_enabled:
-                self.enable_node(node_name)
+            # Store all available versions for given geometry
+            version_num = int(version[1:])
+            versions = self.__name_to_versions.get(geo_name)
+            if versions:
+                versions.append(version_num)
             else:
-                self.disable_node(geo_name + "_v00" + str(version))
+                self.__name_to_versions.update({geo_name: [version_num]})
+
+            self.add_node_reference_param("node_" + node_name, node)
+            yield node
+
+    def update_version(self, curr_version, geo_name, is_enabled):
+        name_prefix = geo_name + "_v"
+        for version in self.__name_to_versions.get(geo_name):
+            if curr_version == version and is_enabled:
+                self.enable_node(name_prefix + str(curr_version).zfill(3))
+            else:
+                self.disable_node(name_prefix + str(version).zfill(3))
